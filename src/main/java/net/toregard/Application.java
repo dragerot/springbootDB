5a30882222743c9e8ffc97203055d414fb64fa02
toregard <drgerot@gmail.com>
2016-04-13 00:12:46 +0200

    Redis added

--- src/main/java/net/toregard/Application.java
+++ src/main/java/net/toregard/Application.java
@@ -1,11 +1,19 @@
 package net.toregard;
 
 
+import net.toregard.model.CV;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 //@Configuration
 //@EnableAutoConfiguration
@@ -22,5 +30,28 @@ public class Application {
 //                .bannerMode(Banner.Mode.CONSOLE)
 //                .run(args);
     }
+
+    @Bean
+    public StringRedisSerializer stringRedisSerializer() {
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        return stringRedisSerializer;
+    }
+
+    @Bean
+    public JacksonJsonRedisSerializer<CV> jacksonJsonRedisJsonSerializer() {
+        JacksonJsonRedisSerializer<CV> jacksonJsonRedisJsonSerializer = new JacksonJsonRedisSerializer<>(CV.class);
+        return jacksonJsonRedisJsonSerializer;
+    }
+
+    @Bean
+    public RedisTemplate<String, CV> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        final RedisTemplate<String,CV> redisTemplate = new RedisTemplate<String, CV>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(stringRedisSerializer());
+        redisTemplate.setValueSerializer(jacksonJsonRedisJsonSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
 
 }
--- src/main/java/net/toregard/controller/RedisController.java
+++ src/main/java/net/toregard/controller/RedisController.java
@@ -2,6 +2,9 @@ package net.toregard.controller;
 
 import net.toregard.fault.BusinessFault;
 import net.toregard.model.CV;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.data.redis.core.ValueOperations;
+
 @RestController
 public class RedisController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String,CV> redisTemplate;
 
     @RequestMapping(value = "/redis/create/{year}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<MessageCV> create(@PathVariable("year") String year) {
+
         MessageCV messageCV= new MessageCV();
         HttpStatus httpStatus=HttpStatus.BAD_REQUEST;
         try{
@@ -21,6 +32,7 @@ public class RedisController {
             messageCV.setCv(createCV(year));
             messageCV.setMessage("OK");
             httpStatus=HttpStatus.OK;
+            saveCVJsonToRedis("springbootDB.CV"+year,messageCV.getCv());
         }catch (BusinessFault businessFault){
             messageCV.setId(businessFault.getId());
             messageCV.setMessage(businessFault.getMessage());
@@ -36,5 +48,19 @@ public class RedisController {
         cv.setType("Kurs");
         cv.setYear(year);
         return  cv;
+    }
+
+    private void saveStringToRedis(String key,String value){
+        ValueOperations<String, String> ops = this.stringRedisTemplate.opsForValue();
+        if (!this.stringRedisTemplate.hasKey(key)) {
+            ops.set(key, value);
+        }
+    }
+
+    private void saveCVJsonToRedis(String key,CV value){
+        ValueOperations<String, CV> ops = this.redisTemplate.opsForValue();
+        if (!this.redisTemplate.hasKey(key)) {
+            ops.set(key, value);
+        }
     }
 }
